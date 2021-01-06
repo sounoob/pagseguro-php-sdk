c92604828ad389ba067912768457ded128434d8a
Sena <github@sounoob.com.br>
2021-01-06 15:35:21 -0300

    Update editPlan.php

--- example/subscription/editPlan.php
+++ example/subscription/editPlan.php
@@ -1,7 +1,7 @@
 <?php
 include '../../vendor/autoload.php';
 
-\Sounoob\pagseguro\config\Config::setAccountCredentials('japle_noodles@hotmail.com', 'F6AEBAA77FF343A4ACA390E76C335511');
+\Sounoob\pagseguro\config\Config::setAccountCredentials('email@domain.com', 'AEBAA77FF343A4ACA390E76C335511F6');
 
 $editPlan = new \Sounoob\pagseguro\subscription\EditPlan('F8A4A4799B9B7FCEE42C0FBBBEB0FBA2');
 $editPlan->setAmountPerPayment('1.23', true);
